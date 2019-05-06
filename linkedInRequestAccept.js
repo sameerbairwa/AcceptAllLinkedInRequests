a76f377c9365b86c1f7acfeac4541931a849cba6
sameerbairwa <sameerbairwa07@gmail.com>
2019-05-07 00:42:25 +0530

    added discription of code

--- linkedInRequestAccept.js
+++ linkedInRequestAccept.js
@@ -1,4 +1,4 @@
-
+// by this script we can accept all our linkedIn requests , just run this script in console of browser
 
 var x  = document.getElementsByClassName('invitation-card__action-btn artdeco-button artdeco-button--secondary ');
 for(var i=0; i<x.length; i++){
